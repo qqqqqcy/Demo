ad31e7e1193da2439dcd1a1f98eb4eeaa07c1808
QQQQQCY <xiajunqcy@163.com>
2018-12-03 23:45:25 +0800

    创建 react-jianshu 项目

---
+++ Project/react-jianshu/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Header from "./components/header";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <h1>hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
